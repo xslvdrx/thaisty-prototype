51f8e7d259ebb66d57338d42c830166d6aeadf79
xslvdrx <105146097+xslvdrx@users.noreply.github.com>
2023-02-01 15:02:27 +0100

    upload1.02

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+//////////////////////////////
+// BACK TO TOP
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (
+		document.body.scrollTop > 200 ||
+		document.documentElement.scrollTop > 200
+	) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 //const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 // const nav = document.querySelector("nav");
